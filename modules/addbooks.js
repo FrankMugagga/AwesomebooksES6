0e9fc8f6b9029e85549234b5edcae0649c11937f
FrankMugagga <frank.mugagga@gmail.com>
2023-02-28 09:53:45 +0300

    add addbooks

--- modules/addbooks.js
+++ modules/addbooks.js
@@ -2,3 +2,8 @@ export default function addBook(book) {
   this.books.push(book);
   this.saveBooksToLocalStorage();
 }
+
+export default addBook() => {
+  this.books.push(book);
+  this.saveBooksToLocalStorage();  
+}
