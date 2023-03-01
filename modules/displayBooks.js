1779e8ba594d9b3ecff7f6739313a9d7d0e459b6
FrankMugagga <frank.mugagga@gmail.com>
2023-03-01 09:58:20 +0300

    corrected eslint errors

--- modules/displayBooks.js
+++ modules/displayBooks.js
@@ -1,18 +1,17 @@
 export default function displayBooks(bookManager, booksList) {
   const books = bookManager.getBooks();
-    booksList.innerHTML = '';  
+  booksList.innerHTML = '';
     books.forEach((book, index) => {
     const li = document.createElement('li');
     li.classList.add('listed');
-    li.innerHTML = `<section class="list_ite"> <div class="book_tit">"${book.title}"</div> <div class="by">by</div> <div class="book_aut"> by ${book.author}</div> </section>`;       
+    li.innerHTML = `<section class="list_ite"> <div class="book_tit">"${book.title}"</div> <div class="by">by</div> <div class="book_aut"> by ${book.author}</div> </section>`;
     const deleteButton = document.createElement('button');
     deleteButton.textContent = 'Delete';
     deleteButton.addEventListener('click', () => {
       bookManager.removeBook(index);
       displayBooks(bookManager, booksList);
-      });  
+    });
     li.appendChild(deleteButton);
     booksList.appendChild(li);
     });
-  }
-  
+  }  
